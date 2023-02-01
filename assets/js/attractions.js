aad737c836320a458d6133fe2ad379374fa2451d
YaszMoon <yasminite@gmail.com>
2023-02-01 20:59:47 +0000

    Add comments to attractions.js. Pull coords from trip API.

--- assets/js/attractions.js
+++ assets/js/attractions.js
@@ -1,46 +1,38 @@
 // Open Weather API
 // Get weather info and longitude and latitude of city
 
-// Grab city name
+// Build weather query
+// TODO: Grab city name from search
 // var city = $("########").val();
 // For testing
 var city = "New York";
-
 var queryURL =
   "https://api.openweathermap.org/data/2.5/weather?q=" +
   city +
   "&appid=" +
   openWeatherAPIKey;
 
-// API query for current weather
+// Ajax for weather
 $.ajax({
   url: queryURL,
   method: "GET",
 }).then(function (weatherResponse) {
   console.log(weatherResponse);
 
+  // Get coordinates for openTrip
   var cityLon = weatherResponse.coord.lon;
   var cityLat = weatherResponse.coord.lat;
 
-console.log(cityLon, cityLat)
-  // Making calls to Open Trip MAP API
+  // Build open trip query
+  // Min and max coordinates to plug into open trip
+  var longitudeMin = cityLon - 0.01;
+  var latitudeMin = cityLat - 0.01;
+  var longitudeMax = cityLon + 0.01;
+  var latitudeMax = cityLat + 0.01;
 
-  // Global variables
-  // User enters a city/place
-  //  Get longitude and latitude for a city using weather API
-  // Save these to variable (min)
-  //
-  40.7143
-  var longitudeMin = cityLon - 0.005;
-  var latitudeMin = cityLat - 0.005;
+  // TODO: Grab category/ies from dropdown ... Add to array?
+  // For testing
   var category = "churches";
-  var longitudeMax = cityLon + 0.005;
-  var latitudeMax = cityLat + 0.005;
-
-  console.log(longitudeMax, latitudeMin)
-
-  // openTripAPIKey in keys.js
-  //  Building query URL
   var openTripAPIURL =
     "https://api.opentripmap.com/0.1/en/places/bbox?lon_min=" +
     longitudeMin +
@@ -55,13 +47,19 @@ console.log(cityLon, cityLat)
     "&format=geojson&apikey=" +
     openTripAPIkey;
 
-    console.log(openTripAPIURL)
-  //  AJAX call
-
+  // Ajax for open trip
   $.ajax({
     url: openTripAPIURL,
     method: "GET",
   }).then(function (tripResponse) {
     console.log(tripResponse);
+
+    // Get coordinates from results
+    for (var i = 0; i < tripResponse.features.length; i++) {
+      var lon = tripResponse.features[i].geometry.coordinates[0];
+      var lat = tripResponse.features[i].geometry.coordinates[1];
+
+      console.log(lon, lat)
+    }
   });
 });
