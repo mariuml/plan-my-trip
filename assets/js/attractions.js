ae1cb4a762a04d3f8486f43d8d4a118dbfe4e32f
YaszMoon <yasminite@gmail.com>
2023-02-02 19:20:45 +0000

    Start adding code for forecast cards

--- assets/js/attractions.js
+++ assets/js/attractions.js
@@ -3,11 +3,11 @@
 
 // Build weather query
 $("#search-button").on("click", function (event) {
-  event.preventDefault()
+  event.preventDefault();
   // TODO: Grab city name from search
   var city = $("#search-input").val();
 
-  console.log(city)
+  console.log(city);
   // For testing
   var queryURL =
     "https://api.openweathermap.org/data/2.5/weather?q=" +
@@ -22,7 +22,7 @@ $("#search-button").on("click", function (event) {
   }).then(function (weatherResponse) {
     console.log(weatherResponse);
 
-    // Get coordinates for openTrip
+    // Get coordinates for openTrip and forecast
     var cityLon = weatherResponse.coord.lon;
     var cityLat = weatherResponse.coord.lat;
 
@@ -64,6 +64,64 @@ $("#search-button").on("click", function (event) {
 
         console.log(lon, lat);
       }
+    });
+
+    var queryURLForecast =
+      "https://api.openweathermap.org/data/2.5/forecast?lat=" +
+      cityLat +
+      "&lon=" +
+      cityLon +
+      "&appid=" +
+      openWeatherAPIKey;
+
+    // API query for forecasted weather
+    $.ajax({
+      url: queryURLForecast,
+      method: "GET",
+    }).then(function (forecastResponse) {
+      //   Check response coming back in console
+      console.log(forecastResponse);
+
+      // Have to repeat this a few times so var
+      var result = forecastResponse.list;
+
+  //     // Forecast shown for 3 -hour blocks for 5 days
+  //     // Loop through in intervals of 8 beginning from 3
+  //     // Gives weather at 12 (midday) each day
+  //     for (var i = 3; i <= 40; i = i + 8) {
+  //       // Create block to house information
+  //       var forecastBlock = $('<div class="forecastBlock">');
+
+  //       // Info for forecast date
+  //       var forecastDate = $("<p>");
+  //       var forecastDateConv = moment(
+  //         result[i].dt_txt,
+  //         "YYYY-MM-D HH:mm:ss"
+  //       ).format("D/MM/YYYY");
+  //       forecastDate.text(forecastDateConv);
+
+  //       // Forecasted weather icon
+  //       var forecastIconURL =
+  //         "https://openweathermap.org/img/wn/" +
+  //         result[i].weather[0].icon +
+  //         ".png";
+  //       var forecastIcon = $("<img>");
+  //       forecastIcon.attr("src", forecastIconURL);
+
+  //       // Info for forecasted temperature
+  //       var forecastTemp = $("<p>");
+  //       var forecastTempConv = result[i].main.temp - 273.15;
+  //       forecastTempConv = forecastTempConv.toFixed(2);
+  //       forecastTemp.text("Temp: " + forecastTempConv + " °C");
+
+  //       // Info for forecasted wind speed
+  //       var forecastWind = $("<p>");
+  //       forecastWind.text("Wind: " + result[i].wind.speed + "KpH");
+
+  //       // Info for forecasted humidty
+  //       var forecastHumidity = $("<p>");
+  //       forecastHumidity.text("Humidity: " + result[i].main.humidity + "%");
+  //     }
     });
   });
 });
