14c71722a5bb9420373c535ffb160abd7265430e
YaszMoon <yasminite@gmail.com>
2023-02-07 20:23:51 +0000

    Format documents

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,21 +1,20 @@
 let zoom = 7;
 // Center location.
 const sendCoords = (lat, lon) => {
-    center.lat = lat;
-    center.lng = lon;
-    zoom = 13;
+  center.lat = lat;
+  center.lng = lon;
+  zoom = 13;
 };
 
 const center = { lat: 51.509865, lng: -0.118092 };
 
-  // Array of pin locations
-  const pinLocations = [];
+// Array of pin locations
+const pinLocations = [];
 
-  // Received titles from Trip advisors API.
-  let titles = [];
-  let searchCityVal = "";
+// Received titles from Trip advisors API.
+let titles = [];
+let searchCityVal = "";
 function initMap() {
-
   // Centers map on a location
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: zoom,
@@ -23,13 +22,17 @@ function initMap() {
   });
   // Loop throw pins
   for (let i = 0; i < pinLocations.length; i++) {
-    const searchWord = titles[i].split(' ').map(word => word.trim());
-    searchWord.push(searchCityVal)
+    const searchWord = titles[i].split(" ").map((word) => word.trim());
+    searchWord.push(searchCityVal);
     // Pin description
     const contentString =
       '<div id="mapContent">' +
-      '<h1 id="mapHeading">'+ titles[i] +'</h1>' +
-      '<div id="mapBodyContent"><a target="_blank" href="https://www.google.com/search?q='+ searchWord.join("+") +'">For more information click here!</a>' +
+      '<h1 id="mapHeading">' +
+      titles[i] +
+      "</h1>" +
+      '<div id="mapBodyContent"><a target="_blank" href="https://www.google.com/search?q=' +
+      searchWord.join("+") +
+      '">For more information click here!</a>' +
       "</div>" +
       "</div>";
     // Pin infoWindow content
@@ -50,7 +53,7 @@ function initMap() {
         map,
       });
     });
-  };
+  }
   // Empty pins array for next call.
   titles = [];
-};
+}
