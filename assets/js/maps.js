38d58ad175916f2b8d15eb171abb3d2b751ecdc2
MarKubil <marius.kub@outlook.com>
2023-02-08 14:55:26 +0000

    Tested new mapApi and deleted GoogleMapApi

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -14,52 +14,6 @@ const pinLocations = [];
 // Received titles from Trip advisors API.
 let titles = [];
 
-
-// function initMap() {
-//   // Centers map on a location
-//   const map = new google.maps.Map(document.getElementById("map"), {
-//     zoom: zoom,
-//     center: center,
-//   });
-//   // Loop throw pins
-//   for (let i = 0; i < pinLocations.length; i++) {
-//     const searchWord = titles[i].split(" ").map((word) => word.trim());
-//     searchWord.push(searchCityVal);
-//     // Pin description
-//     const contentString =
-//       '<div id="mapContent">' +
-//       '<h1 id="mapHeading">' +
-//       titles[i] +
-//       "</h1>" +
-//       '<div id="mapBodyContent"><a target="_blank" href="https://www.google.com/search?q=' +
-//       searchWord.join("+") +
-//       '">For more information click here!</a>' +
-//       "</div>" +
-//       "</div>";
-//     // Pin infoWindow content
-//     const infowindow = new google.maps.InfoWindow({
-//       content: contentString,
-//       ariaLabel: titles[i],
-//     });
-//     // Pin location
-//     const marker = new google.maps.Marker({
-//       position: pinLocations[i],
-//       map,
-//       title: titles[i],
-//     });
-
-//     marker.addListener("click", () => {
-//       infowindow.open({
-//         anchor: marker,
-//         map,
-//       });
-//     });
-//   }
-//   // Empty pins array for next call.
-//   titles = [];
-// }
-
-
 // Map viewpoint and layer
 var map = L.map("map").setView(center, zoom);
 L.tileLayer('https://api.maptiler.com/maps/streets-v2/256/{z}/{x}/{y}.png?key=KWg28OjzL3RlPD1tpGPc', {
